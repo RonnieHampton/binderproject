ca05575420795ffcf5d9b1864ace860ecb4febd6
Ronnie Hampton <teddybrosevelt8@gmail.com>
2026-05-20 04:16:44 -0400

    A lot of Drag and Drop implementations.

--- src/components/Binder.tsx
+++ src/components/Binder.tsx
@@ -1,19 +1,32 @@
-import {useDroppable} from '@dnd-kit/react';
+import BinderDroppable from "./BinderDroppable";
+import type { CardInstance } from "../types/scryfall";
+import { useState } from "react";
 
 type BinderProps = {
-  id: string;
-  children?: React.ReactNode;
+  cards: (CardInstance | null)[]
 }
 
-function Binder({id, children}: BinderProps) {
-  const {ref} = useDroppable({
-    id,
-  });
+
+function Binder({ cards }: BinderProps) {
+  const [page, setPage] = useState(0);
+  const start = page * 12;
+  const end = start + 12;
 
   return (
-    <div ref={ref} style={{width: 300, height: 300, backgroundColor: 'lightgray'}}>
-      {children}
+    <>
+    <div style={{
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gap: "12px",
+  }}>
+    {cards.slice(start, end).map((card, index) => (
+      <BinderDroppable key={index} card={card} index={index + (page * 12)} />
+    ))}
+
     </div>
+    <button onClick={() => setPage(page - 1)} disabled={page === 0}> Previous Page </button>
+    <button onClick={() => setPage(page + 1)} disabled={page === 4}> Next Page </button>
+    </>
   );
 }
 
